0dd15510959a318c0f7856dde8145bad7f5e612b
Rudra Verma <rudraverma781@gmail.com>
2026-01-19 18:57:41 +0530

    updated the old code and added functionalities

--- frontend/src/pages/Pricing.tsx
+++ frontend/src/pages/Pricing.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { appPlans } from '../assets/assets';
 
 interface Plan{
   id : string;
@@ -10,7 +11,19 @@ interface Plan{
 }
 
 export default function Pricing() {
+
+  const [plans] = useState<Plan[]>(appPlans)
   return (
-    <div>Pricing</div>
+    <>
+     <div className="w-full max-w-5xl mx-auto z-20 max-md:px-4 min-h-[80vh]">
+
+        <div className="text-center mt-16 ">
+          <h2 className="text-gray-100 text-3xl font-medium">Choose Your Plan</h2>
+          <p className="text-gray-400 text-sm max-w-md mx-auto mt-2 ">
+            Start for free and scale up as you grow . Find the perfect plan for your content creation needs.
+          </p>
+        </div>
+     </div>
+    </>
   )
 }
